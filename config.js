5e5e72e802183d27b8da8a1deb9773db0be441cb
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:46:25 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PIXIES';
+export let nameSongs = [
+	"WHERE IS MY MIND?", /*1*/
+	"DEBASER", /*2*/
+	"HEY", /*3*/
+	"ALL I THINK ABOUT NOW", /*4*/
+	"MONKEY GONE TO HEAVEN", /*5*/
+	"GOUGE AWAY", /*6*/
+	"NO 13 BABY", /*7*/
+	"THE HAPPENING", /*8*/
+	"ALL OVER THE WORLD", /*9*/
+	"IS SHE WEIRD", /*10*/
+	"I BLEED", /*11*/
+	"MR. GRIEVES", /*12*/
+	"CARIBOU", /*13*/
+	"BONE MACHINE", /*14*/
+	"HERE COMES YOUR MAN", /*15*/
+	"SUBBACULTCHA", /*16*/
+	"BROKEN FACE", /*17*/
+	"GIGANTIC", /*18*/
+	"WAVE OF MUTILATION", /*19*/
+	"TAME", /*20*/
+]
